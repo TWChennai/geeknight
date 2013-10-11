0ff1364c681b044e8bc95e1cd3523f778c835f30
Subhas Dandapani <rdsubhas@gmail.com>
2013-10-11 11:21:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Geek Night Chennai
+
+an open forum for geeks to connect, discuss &amp; learn latest ideas, technologies and trends in software development
---
+++ js/main.js
@@ -0,0 +1,57 @@
+var targetDate = new Date("Oct 24 2013 17:00")
+    targetTS = targetDate.getTime(),
+    $d = document.getElementById("days"),
+    $h = document.getElementById("hours"),
+    $m = document.getElementById("minutes"),
+    $s = document.getElementById("seconds"),
+    $tick = document.getElementById("tick"),
+    $tock = document.getElementById("tock");
+
+function updateCountDown() {
+  var currentDate = new Date().getTime();
+  var secondsLeft = (targetTS - currentDate) / 1000;
+
+  var days = parseInt(secondsLeft / 86400);
+  secondsLeft = secondsLeft % 86400;
+
+  var hours = parseInt(secondsLeft / 3600);
+  secondsLeft = secondsLeft % 3600;
+
+  var minutes = parseInt(secondsLeft / 60);
+  var seconds = parseInt(secondsLeft % 60);
+
+  $d.innerHTML = days;
+  $h.innerHTML = hours;
+  $m.innerHTML = minutes;
+
+  if (seconds % 2 == 0) {
+    $tick.innerHTML = seconds;
+    $s.className = "";
+  } else {
+    $tock.innerHTML = seconds;
+    $s.className = "odd";
+  }
+}
+
+updateCountDown();
+setInterval(updateCountDown, 1000);
+
+var twLocation = [ 12.98398, 80.24626], mapLoaded = false;
+
+var map = L.map('map', {
+  scrollWheelZoom: false,
+  center: twLocation,
+  zoom: 17
+});
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).on('tileload', function() {
+  if (!mapLoaded) {
+    document.getElementById("location").style.display = 'none';
+    L.marker(twLocation).addTo(map)
+      .bindPopup(document.getElementById("address"))
+    .openPopup();
+    mapLoaded = true;
+  }
+}).addTo(map)
