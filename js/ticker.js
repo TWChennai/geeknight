0d5357448d2ec64b0f36d4070cab043c5eed06e6
Subhas Dandapani <rdsubhas@gmail.com>
2013-10-11 19:27:48 +0530

    Optimized compiled CSS

--- js/ticker.js
+++ js/ticker.js
@@ -34,23 +34,3 @@ function updateCountDown() {
 
 updateCountDown();
 setInterval(updateCountDown, 1000);
-
-var twLocation = [ 12.98398, 80.24626 ], mapLoaded = false;
-
-var map = L.map('map', {
-  scrollWheelZoom: false,
-  center: twLocation,
-  zoom: 17
-});
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).on('tileload', function() {
-  if (!mapLoaded) {
-    document.getElementById("location").style.display = 'none';
-    L.marker(twLocation).addTo(map)
-      .bindPopup(document.getElementById("address"))
-    .openPopup();
-    mapLoaded = true;
-  }
-}).addTo(map)
