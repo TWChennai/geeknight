569a5255e05e6260ab9bd2a836e7961be2c590be
niksw7 <niksw7@gmail.com>
2015-03-19 23:15:59 +0530

    Changes to deploy

--- go/deploy.sh
+++ go/deploy.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Reconstituting SSH key..."
-echo $DEPLOY_KEY | sed 's/\\n/\'$'\n/g' > id_geeknight
-chmod 600 id_geeknight
+cd $SNAP_WORKING_DIR
 
-export GIT_SSH=$(pwd)/go/ssh.sh
-export SSH_KEY=$(pwd)/id_geeknight
-
-set -xe
-
-echo "Cloning gh-pages..."
-rm -Rf gh-pages
-git config --global user.email "snap@snap-ci.org"
-git config --global user.name "snap-ci"
-git clone --quiet --branch=gh-pages git@github.com:TWChennai/geeknight.git gh-pages
+echo "Installing Gems..."
+bundle install
 
 echo "Building site..."
 bundle exec nanoc
 
+echo "Fetching gh-pages..."
+git fetch origin gh-pages
+git checkout --track origin/gh-pages
+cp -r gh-pages/* ./
+rm -rf gh-pages tmp
+
 echo "Checking for changes..."
-cd gh-pages
 if [[ -z $(git status -s) ]]; then
   echo "No changes to push"
   exit 0
@@ -29,6 +24,3 @@ echo "Pushing result..."
 git add --all .
 git commit --amend --allow-empty -q -m "Built $SNAP_COMMIT_SHORT"
 git push -fq origin gh-pages
-
-echo "Cleaning up..."
-rm -Rf ~/.ssh/id_geeknight*
