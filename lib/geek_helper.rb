92d2436968f47439b2aeb0a4e49b015ebdbf6a06
Arun Kumar Sundaramurthy <arunkumarsundaramurthy@gmail.com>
2016-10-04 13:06:12 +0530

    Testing: added json-ld for the event for google rich card

--- lib/geek_helper.rb
+++ lib/geek_helper.rb
@@ -15,4 +15,12 @@ module GeekHelper
   def latest
     @@first ||= events.first
   end
+
+  def json_of_event(item)
+    {'@context' => 'http://schema.org', '@type' => 'EducationEvent', 'name' => "Geek Night Chennai - #{Time.parse(item[:start]).strftime("%b %Y")}",
+     'startDate' => Time.parse(item[:start]).iso8601, 'url' => "http://twchennai.github.io/geeknight#{item.path}",
+     'location' => {'@type' => 'Place', 'name' => 'ThoughtWorks', 'address' => {
+        '@type' => 'PostalAddress', 'addressLocality' => 'Chennai', 'addressRegion' => 'Tamil Nadu', 'postalCode' => '600113', 'streetAddress' => 'Ascendas'
+    }}}.to_json
+  end
 end
