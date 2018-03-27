a0a5c3d00f2386de3e12f97e9758452d1e64f6b0
Vikhyath <vikhyatc@thoughtworks.com>
2018-03-27 18:18:11 +0530

    [Viky] Highlighting active menu on scroll

--- content/static/js/app.js
+++ content/static/js/app.js
@@ -1,3 +1,5 @@
+var lastId,topMenu,topMenuHeight, menuItems,scrollItems;
+
 function getAgenda(event, id, value) {
 	$('.speakers').removeClass("active");
 	$('.event').removeClass("active");
@@ -13,15 +15,60 @@ function getEventsFor(id, value) {
 	$("*[id*="+value+"]").addClass("show");
 }
 
+function setClickListiners() {
+	topMenu = $("#menu"),
+    topMenuHeight = topMenu.outerHeight()+30,    
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+	menuItems.click(function(e) {
+	 	var href = $(this).attr("href"),
+	 	offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+		$('html, body').stop().animate({ 
+	     	scrollTop: offsetTop
+	  	}, 300);
+		e.preventDefault();
+	});
+}
+
+function highlightMenu() {
+	var fromTop = $(this).scrollTop()+topMenuHeight;
+
+	if(!scrollItems) return;
+      
+	var cur = scrollItems.map(function(){
+	 if ($(this).offset().top < fromTop)
+	   return this;
+	});   
+	cur = cur[cur.length-1];
+	var id = cur && cur.length ? cur[0].id : "";
+
+	if (lastId !== id) {
+	   lastId = id;
+	   menuItems.parent()
+	   			.removeClass("active")
+	   			.end()
+	   			.filter("[href='#"+id+"']")
+	   			.parent()
+	   			.addClass("active");
+	}
+}
+
 $(document).ready(function() {
 	$($(".months")[0].childNodes[1]).addClass("active");
+	setClickListiners();
+	highlightMenu();
 });
 
 $(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 600) {
-        $('#header').addClass('shadow');
-    } else {
-        $('#header').removeClass('shadow')
-    }
+	var y = $(this).scrollTop();
+	if(y > 600) {
+		$("#header").addClass("shadow");		
+	} else {
+		$("#header").removeClass("shadow");
+	}
+	highlightMenu();
 });
