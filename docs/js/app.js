2ae1589be4390c4e823163d30c534947f738eda8
Vikhyath <vikhyatc@thoughtworks.com>
2018-03-27 18:38:36 +0530

    [Viky] Fixing about section highlight

--- docs/js/app.js
+++ docs/js/app.js
@@ -17,43 +17,43 @@ function getEventsFor(id, value) {
 
 function setClickListiners() {
 	topMenu = $("#menu"),
-    topMenuHeight = topMenu.outerHeight()+30,    
-    menuItems = topMenu.find("a"),
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
+	topMenuHeight = topMenu.outerHeight()+30,    
+	menuItems = topMenu.find("a"),
+	scrollItems = menuItems.map(function(){
+		var item = $($(this).attr("href"));
+		if (item.length) { return item; }
+	});
 
 	menuItems.click(function(e) {
 	 	var href = $(this).attr("href"),
 	 	offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
 		$('html, body').stop().animate({ 
-	     	scrollTop: offsetTop
-	  	}, 300);
+			scrollTop: offsetTop
+		}, 300);
 		e.preventDefault();
 	});
 }
 
 function highlightMenu() {
-	var fromTop = $(this).scrollTop()+topMenuHeight;
+	var fromTop = $(this).scrollTop()+(topMenuHeight*4);
 
 	if(!scrollItems) return;
-      
+
 	var cur = scrollItems.map(function(){
-	 if ($(this).offset().top < fromTop)
-	   return this;
-	});   
+	if ($(this).offset().top < fromTop)
+		return this;
+	});
 	cur = cur[cur.length-1];
 	var id = cur && cur.length ? cur[0].id : "";
 
 	if (lastId !== id) {
-	   lastId = id;
-	   menuItems.parent()
-	   			.removeClass("active")
-	   			.end()
-	   			.filter("[href='#"+id+"']")
-	   			.parent()
-	   			.addClass("active");
+		lastId = id;
+		menuItems.parent()
+				.removeClass("active")
+				.end()
+				.filter("[href='#"+id+"']")
+				.parent()
+				.addClass("active");
 	}
 }
 
